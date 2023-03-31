5bb30997401170c8b9d75a646fa21b7712983d45
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-31 14:21:59 +0300

    function

--- src/index.js
+++ src/index.js
@@ -10,27 +10,63 @@ const countryInfo = document.querySelector('.country-info');
 
 searchBox.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
-
 //Поле пошуку
 function onInputSearch(event) {
-  const searchValue = searchBox.ariaValueMax.trim();
+  const searchValue = searchBox.value.trim();
   countryList.innerHTML = '';
   countryInfo.innerHTML = '';
   if (!searchValue) return;
+
+  //інтерфейс
+  //Якщо у відповіді бекенд повернув більше ніж 10 країн, в інтерфейсі
+  fetchCountries(searchValue).then(result => {
+    if (result.length > 10) {
+      Notify.info(
+        'Too many matches found. Please, enter a more specific name.'
+      );
+      return;
+    }
+    renderedCountries(result);
+  });
 }
-/*Інтерфейс */
-//fetchCountries();
+function renderedCountries(result) {
+  const input = result.length;
 
+  if (inputLetters === 1) {
+    countryList.innerHTML = '';
+    countryCardMarkup(result);
+  }
+  //Якщо бекенд повернув від 2-х до 10-и країн,
+  if (input > 1 && input <= 10) {
+    countryInfo.innerHTML = '';
+    countriesList(result);
+  }
+}
+function countriesList(result) {
+  const listMarkup = result
+    .map(({ name, flags }) => {
+      return `<li>
+                         <img src="${flags.svg}" alt="${name}" width="60" height="auto">
+                         <span>${name.official}</span>
+                 </li>`;
+    })
+    .join('');
+  countryList.innerHTML = listMarkup;
+  return listMarkup;
+}
 
-/*Фільтрація полів
-//function countryList()
-//name.official - повна назва країни
-capital - столиця
-population - населення
-flags.svg - посилання на зображення прапора
-languages - масив мов
-
-додай рядок параметрів запиту - таким чином цей бекенд реалізує фільтрацію полів.*/
-
-//Обробка помилки
-//fetchCountries.js
+function countryCardMarkup(result) {
+  const cardMarkup = result
+    .map(({ flags, name, capital, population, languages }) => {
+      languages = Object.values(languages).join(', ');
+      return `
+             <img src="${flags.svg}" alt="${name}" width="320" height="auto">
+             <p> ${name.official}</p>
+             <p>Capital: <span> ${capital}</span></p>
+             <p>Population: <span> ${population}</span></p>
+             <p>Languages: <span> ${languages}</span></p>`;
+    })
+    .join('');
+  countryInfo.innerHTML = cardMarkup;
+  return cardMarkup;
+}
