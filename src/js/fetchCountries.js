a63d0a22fbc1a1cf67da930ddf67d47732b5aa96
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-04-03 13:00:55 +0300

    const fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,13 @@
-export const fetchCountries = (name) => {
+export const fetchCountries = name => {
   const params = new URLSearchParams({
     fields: `name,capital,population,flags,languages,`,
   });
-  const response = fetch('https://restcountries.com/v3.1/name/${name}?${params}');
-  if (!response.ok) {
-    throw new Error(response.status);
-  }
-  return response.json();
+  return fetch(`https://restcountries.com/v3.1/name/${name}?${params}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
 };
