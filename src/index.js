62d4681a0ce6539e6dda4cd1b817db1c65efad05
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-04-03 12:52:51 +0300

    function

--- src/index.js
+++ src/index.js
@@ -17,51 +17,50 @@ function onInputSearch(event) {
   countryList.innerHTML = '';
   countryInfo.innerHTML = '';
   if (!searchValue) return;
-}
-//Інтерфейс
 
+  fetchCountries(searchValue)
+    .then(result => {
+      function renderedCountries(result) {
+        const inputLetters = result.length;
 
-fetchCountries(searchValue)
-  .then(result => { 
-  
-//function renderedCountries(result) {
-  const inputLetters = result.length;
+        if (inputLetters === 1) {
+          countryList.innerHTML = '';
+          countryCardMarkup(result);
+        }
 
-  if (inputLetters === 1) {
-    countryList.innerHTML = '';
-    countryCardMarkup(result);
-  }
-  //Якщо бекенд повернув від 2-х до 10-и країн,
-  if (inputLetters > 1 && inputLetters <= 10) {
-    countryInfo.innerHTML = '';
-    countriesListMarkup(result);
-  }
-    if (result.length > 10) {
-      Notify.info(
-        'Too many matches found. Please, enter a more specific name.'
-      );
-      return;
-    }
-    renderedCountries(result);
-  })
-  .catch(error => {
-    Notify.failure('Oops, there is no country with that name');
-},
+        if (inputLetters > 2 && inputLetters <= 10) {
+          countryInfo.innerHTML = '';
+          countriesListMarkup(result);
+        }
 
-//Фільтрація полів
+        if (result.length > 10) {
+          Notify.info(
+            'Too many matches found. Please, enter a more specific name.'
+          );
+          return;
+        }
+      }
+      renderedCountries(result);
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+    });
 
-function countriesListMarkup(result) {
-  const listMarkup = result
-    .map(({ name, flags }) => {
-      return `<li>
+  //Фільтрація полів
+
+  function countriesListMarkup(result) {
+    const listMarkup = result
+      .map(({ name, flags }) => {
+        return `<li>
                          <img src="${flags.svg}" alt="${name}" width="60" height="auto">
                          <span>${name.official}</span>
                  </li>`;
-    })
-    .join('');
-  countriesListMarkup.innerHTML = listMarkup;
-  return listMarkup;
-},
+      })
+      .join('');
+    countriesListMarkup.innerHTML = listMarkup;
+    return listMarkup;
+  }
+}
 
 function countryCardMarkup(result) {
   const cardMarkup = result
@@ -77,4 +76,5 @@ function countryCardMarkup(result) {
     .join('');
   countryInfo.innerHTML = cardMarkup;
   return cardMarkup;
-})
+}
+
